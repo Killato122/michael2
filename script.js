0337550eceb0b049b0ff15c060c5c49507d7cd67
Killato122 <mike.albanografico@gmail.com>
2024-07-30 18:39:38 +0200

    suoni

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ document.addEventListener("DOMContentLoaded", function() {
   const noButton = document.getElementById('no');
   const yesButton = document.getElementById('yes');
 
-  // Gestisce il clic sul pulsante "No"
-  noButton.addEventListener('click', function() {
-      noButton.innerHTML = '😭'; // Sostituisce il testo con l'emoji triste
-      playSound('https://github.com/Killato122/michael2/blob/main/Sad%20Hamster%20Violin%20Meme%20Full.mp3'); // Suona un effetto sonoro triste
-  });
+    // Gestisce il clic sul pulsante "No"
+    noButton.addEventListener('click', function() {
+        noButton.innerHTML = '😭'; // Sostituisce il testo con l'emoji triste
+        playSound('https://raw.githubusercontent.com/Killato122/sito-amore/main/Sad%20Hamster%20Violin%20Meme%20Full.mp3'); // Suona un effetto sonoro triste
+    });
 
   // Gestisce il clic sul pulsante "Sì"
   yesButton.addEventListener('click', function() {
